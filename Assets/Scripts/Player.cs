405b1b06b7c186dae680b8dd0ceb05d4bbcd8e2d
Kodiererin <iamujjwal45@gmail.com>
2024-01-27 00:32:49 +0530

    Adding Ground

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,10 +2,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+
+    public  Sprite[] sprites;
+    private int spriteIndex;
     private Vector3 direction;
     public float gravity = -9.8f;
 
-    private float strength= 5f;
+    public float strength= 5f;
+
+
+
+    private void Awake(){
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start(){
+        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+    }
 
     private void Update(){
         if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
@@ -23,10 +37,21 @@ public class Player : MonoBehaviour
         direction.y += gravity * Time.deltaTime;
         transform.position += direction * Time.deltaTime;  
 
-        
 
 
 
+
+    }
+
+
+    private void AnimateSprite(){
+       spriteIndex++;
+
+       if(spriteIndex >= sprites.Length){
+           spriteIndex = 0;
+       }
+
+       spriteRenderer.sprite = sprites[spriteIndex];
     }
 }
  
